ddbbd87fbec036705edb77b4167821531aaa1e8f
SANDHIYA VELUMANI <64846988+Sandhiya-251299@users.noreply.github.com>
2020-06-10 21:24:28 +0530

    Create Damdesign.java

---
+++ Damdesign.java
@@ -0,0 +1,29 @@
+Virtusa Test-3 - Dam Design
+class Result {
+
+public static int maxHeight(List<Integer> wallPositions, List<Integer> wallHeights) {
+
+int l=0;
+for (int i=0; i<wallPositions.size()-1; i++) {
+if (wallPositions.get(i)<wallPositions.get(i+1)-1) {
+int height = Math.abs(wallHeights.get(i+1) - wallHeights.get(i));
+int gap = wallPositions.get(i+1) - wallPositions.get(i) - 1;
+int localMax = 0;
+if (gap > height) {
+int low = Math.max(wallHeights.get(i+1), wallHeights.get(i)) + 1;
+int remainingGap = gap - height - 1;
+localMax = low + remainingGap/2;
+
+} else {
+localMax = Math.min(wallHeights.get(i+1), wallHeights.get(i)) + gap;
+}
+
+l= Math.max(l, localMax);
+}
+}
+
+return l;
+
+}
+
+}
