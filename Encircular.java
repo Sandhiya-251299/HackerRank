61c330fa366805c4fb340660da96cd48ce89ebcc
SANDHIYA VELUMANI <64846988+Sandhiya-251299@users.noreply.github.com>
2020-06-11 18:47:20 +0530

    Create Encircular.java

---
+++ Encircular.java
@@ -0,0 +1,34 @@
+public class Encircular {
+    static String doesCircleExist(String commands) {
+        int x = 0;
+        int y = 0;
+        boolean goUp = false;
+        for (int i = 0; i < commands.length(); i++) {
+            switch (commands.charAt(i)) {
+                case 'G' :
+                    if (goUp) {
+                        y++;
+                    } else {
+                        x++;
+                    }
+                    break;
+                case 'L' :
+                    goUp = true;
+                    break;
+                case 'R' :
+                    goUp = false;
+                    break;
+            }
+        }
+        if (x == 0 && y == 0) {
+            return "YES";
+        }
+        return "NO";
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(doesCircleExist("GLGLG"));
+        System.out.println(doesCircleExist("GRGL"));
+    }
+}
